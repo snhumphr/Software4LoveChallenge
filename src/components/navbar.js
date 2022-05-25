362190da9933673a6ae12bcc4a3726f32ab06b06
myNAme <snhumphr@ualberta.ca>
2022-05-24 20:34:13 -0600

    Made the navbar look better

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,17 +4,18 @@ import { Link } from "gatsby"
 
 export const NavBar = ({ pages}) => {
   return (
-    <div
+    <h1
       style={{
         margin: `0 auto`,
         padding: `var(--space-4) var(--size-gutter)`,
         display: `flex`,
         alignItems: `center`,
         justifyContent: `space-between`,
+		backgroundColor: 'mediumpurple',
       }}
     >
-	 {pages.map((page) => (<Link to={"/"+page[1]}>{page[0]}</Link>))}
-    </div>
+	 {pages.map((page) => (<Link style={{margin:'0 75px 0 75px', backgroundColor: 'lightblue'}} to={"/"+page[1]}>{page[0]}</Link>))}
+    </h1>
   )
 }
 
